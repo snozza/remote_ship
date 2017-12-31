fe727eeccc3d412b728ff1834043d841c9169bd4
Andrew <andrewcsnead@gmail.com>
2017-12-31 11:57:42 +0000

    add simple move methods

--- src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
+++ src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
@@ -2,4 +2,28 @@ package com.packtpublishing.tddjava.ch04ship;
 
 public class Ship {
 
+    private final Location location;
+    public Location getLocation() {
+        return location;
+    }
+
+    public Ship(Location location) {
+        this.location = location;
+    }
+
+    public boolean moveForward() {
+        return location.forward();
+    }
+
+    public boolean moveBackward() {
+        return location.backward();
+    }
+
+    public void turnLeft() {
+        location.turnLeft();
+    }
+
+    public void turnRight() {
+        location.turnRight();
+    }
 }
--- src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -7,4 +7,44 @@ import static org.testng.Assert.*;
 @Test
 public class ShipSpec {
 
+    private Ship ship;
+    private Location location;
+
+    @BeforeMethod
+    public void beforeTest() {
+        location = new Location(new Point(21, 13), Direction.NORTH);
+        ship = new Ship(location);
+    }
+
+    public void whenInstantiatedThenLocationIsSet() {
+        assertEquals(ship.getLocation(), location);
+    }
+
+    public void whenMoveForwardThenForward() {
+        Location expected = location.copy();
+        expected.forward();
+        ship.moveForward();
+        assertEquals(ship.getLocation(), expected);
+    }
+
+    public void whenMoveBackwardThenBackward() {
+        Location expected = location.copy();
+        expected.backward();
+        ship.moveBackward();
+        assertEquals(ship.getLocation(), expected);
+    }
+
+    public void whenTurnLeftThenLeft() {
+        Location expected = location.copy();
+        expected.turnLeft();
+        ship.turnLeft();
+        assertEquals(ship.getLocation(), expected);
+    }
+
+    public void whenTurnRightThenRight() {
+        Location expected = location.copy();
+        expected.turnRight();
+        ship.turnRight();
+        assertEquals(ship.getLocation(), expected);
+    }
 }
